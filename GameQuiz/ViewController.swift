d5d2ce4ddb8c8273d29a8b30406762c7d96638c7
Bắc Vương Toàn <43468251+vuongbac@users.noreply.github.com>
2020-09-02 23:56:33 +0700

    Initial Commit

---
+++ GameQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameQuiz
+//
+//  Created by Vương Toàn Bắc on 9/2/20.
+//  Copyright © 2020 VTB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
